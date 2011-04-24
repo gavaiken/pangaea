f1d8260a9786966fb7d117eea735257eef78ab4a
Gav Aiken <gav.aiken@gmail.com>
2011-04-24 10:43:23 +0100

    Basic Entry Point

---
+++ src/aiken/gav/appengine/pangaea/client/Pangaea.java
@@ -0,0 +1,13 @@
+package aiken.gav.appengine.pangaea.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class Pangaea implements EntryPoint {
+
+  @Override
+  public void onModuleLoad() {
+    // TODO Auto-generated method stub
+
+  }
+
+}
